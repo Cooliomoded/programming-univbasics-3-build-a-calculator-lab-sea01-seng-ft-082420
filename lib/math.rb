1e5ad81a55a96fdc366a8035f9e5047c76583521
Davis Brown <cooliomoded@gmail.com>
2020-08-06 18:48:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  puts difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  puts quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  puts product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  puts remainder
 end
 
 def square_root(num)
-
+  rootof = Math.sqrt(num)
+  puts rootof
 end
